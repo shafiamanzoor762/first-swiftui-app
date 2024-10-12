51d9c8d414d90a495268f47bca8b21674d2660e8
shafia <shafiamanzoor762@gmail.com>
2024-10-12 12:37:40 +0500

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by apple on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
